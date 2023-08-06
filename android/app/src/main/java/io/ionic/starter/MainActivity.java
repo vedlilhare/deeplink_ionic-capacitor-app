a5fd22d54450863db8e07c6830cb28c5e14d3e22
vedlilhare <vedlilhare@gmail.com>
2023-08-06 15:14:36 +0530

    deeplink integration with branch IO for Android

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,24 @@
-package io.ionic.starter;
+ package io.ionic.starter;
+  import android.content.Intent;
+ import android.os.Bundle;
+ import co.boundstate.BranchDeepLinks;
+ import com.getcapacitor.BridgeActivity;
+ import com.getcapacitor.Plugin;
+ import java.util.ArrayList;
+ import io.branch.referral.Branch;
 
-import com.getcapacitor.BridgeActivity;
+public class MainActivity extends BridgeActivity {
 
-public class MainActivity extends BridgeActivity {}
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+   registerPlugin(BranchDeepLinks.class);
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    this.setIntent(intent);
+    super.onNewIntent(intent);
+ }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component , NgZone  } from '@angular/core';
 import { Router } from '@angular/router';
 import { App, URLOpenListenerEvent } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
+import { BranchDeepLinks, BranchInitEvent } from 'capacitor-branch-deep-links';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +10,40 @@ import { App, URLOpenListenerEvent } from '@capacitor/app';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private router: Router, private zone: NgZone) {
+  constructor(private router: Router, private zone: NgZone , public platform: Platform) {
 
 		this.initializeApp();
   }
 
     initializeApp() {
-      App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
-        this.zone.run(() => {
-          const domain = 'maheshbillore.online';
+      // App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      //   this.zone.run(() => {
+      //     const domain = 'maheshbillore.online';
   
-          const pathArray = event.url.split(domain);
-          // The pathArray is now like ['https://maheshbillore.online', '/details/42']
+      //     const pathArray = event.url.split(domain);
+      //     // The pathArray is now like ['https://maheshbillore.online', '/details/42']
   
-          // Get the last element with pop()
-          const appPath = pathArray.pop();
-          if (appPath) {
-            this.router.navigateByUrl(appPath);
-          }
-        });
-      });
+      //     // Get the last element with pop()
+      //     const appPath = pathArray.pop();
+      //     if (appPath) {
+      //       this.router.navigateByUrl(appPath);
+      //     }
+      //   });
+      // });
+      this.platform.ready().then(() => {
+      BranchDeepLinks.addListener('init', (event: BranchInitEvent) => {
+               // Retrieve deeplink keys from 'referringParams' and evaluate the values to determine where to route the user
+               // Check '+clicked_branch_link' before deciding whether to use your Branch routing logic
+                console.log(event ,"deeplink data log");
+                // alert(event);
+                 alert("Open app with a Branch deep link: " + JSON.stringify(event));
+
+               });
+        
+               BranchDeepLinks.addListener('initError', (error: any) => {
+                 console.error(error);
+               });
+              });
+        
     }
   }
